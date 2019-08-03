59a5c1281543ad5ad3aa49820738661d698c0d8a
ExistentialMisery <momynov.sultan@gmail.com>
2019-08-04 02:34:37 +0600

    initial commit

---
+++ src/ForLoop.java
@@ -0,0 +1,15 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        byte i = 1;
+        for (; i > 0; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+// continue - to next interaction
+// else
+// break - stop the loop
+// fori - IDEA shortcut
---
+++ src/MainClass.java
@@ -0,0 +1,35 @@
+
+public class MainClass {
+    public static void main(String[] args){
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        b = 127;
+        b++;
+        System.out.println(b);
+        i = 20/11;
+        System.out.println(i);
+
+        float f;
+        double d;
+
+        f = 23425.12312423525f;
+        d = 23425.12312423525;
+
+        System.out.println(f);
+        System.out.println(d);
+
+        char c;
+        boolean bool;
+
+        c = 'z'; // unicode
+        c++;
+
+        System.out.println(c);
+
+
+
+    }
+}
