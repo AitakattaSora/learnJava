865317be2c69aeb62bfe8adab9ca1f9795015132
ExistentialMisery <momynov.sultan@gmail.com>
2019-10-26 19:07:57 +0600

    session 2

---
+++ src/Input.java
@@ -0,0 +1,22 @@
+//      Урок 7: Ввод данных. Класс Scanner.
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        /*
+         * Scanner - класс
+         * new Scanner - объект класса Scanner
+         * System.in - стандартный входной поток
+         * Теперь s ссылается на объект класса Scanner
+        */
+
+        Scanner s = new Scanner(System.in); // new - ключевое слово, которое обозначает создание нового объекта класса
+        System.out.println("Enter something");
+        String myString = s.nextLine(); // nextLine - метод класса Scanner, который считывае 1 строчку чего-либо
+        System.out.println("You have entered: " + myString);
+
+
+    }
+}
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,2 +1,24 @@
+//      Урок 4: Цикл while.
+
 public class WhileLoops {
+    public static void main(String[] args) {
+        int i = 3;
+        boolean b = true;
+        boolean greater = i > 3;
+        boolean equal = i == 5; // для сравнения двух чисел на равенство используется оператор "==".
+                            // Не "=" т.к он уже зарезрвирован под оператор присваивания
+        System.out.println(b);
+        System.out.println(greater);
+        System.out.println(equal);
+/*
+* Цикл While выполняется до тех пор, пока условие цикла верно
+*   В круглых скобках пишется условие выполнения
+*   В квадратных скобках пишется тело цикла (что будет выполняться пока условие верно)
+* */
+        // все операторы в Java пишутся с маленькой буквы
+        while(i < 10) {
+            System.out.println(i + ": Hello");
+            i = i + 1;
+        }
+    }
 }
