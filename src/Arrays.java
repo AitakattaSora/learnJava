e4874e07bb011650c6e120e9cb3ad0cad4f26cd8
ExistentialMisery <momynov.sultan@gmail.com>
2019-10-27 23:22:01 +0600

    day 3 (lessons 8-11)

---
+++ src/Arrays.java
@@ -0,0 +1,34 @@
+//      Урок 11: Массивы в Java.
+
+import java.sql.SQLOutput;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        char character = 'a'; // примитив
+        String s = "Hello"; // <=> String s = new String("Hello") // ссылочный
+
+        int number = 10; // примитивный тип данных  // [10] (коробка)
+
+//        Создание массива
+        int[] numbers = new int[5]; // ссылочный    // numbers ссылается на [массив]
+
+//        По умолчанию все элементы массива имеют значение 0, если это целочисленный массив
+        System.out.println(numbers[0] + ", " + numbers[3] + ", " + numbers[4]);
+
+        System.out.println();       // вывод пустой строки
+
+//        Цикл for отлично подходит для инициализации элементов всего массива
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = i;
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("второй спобоб инициализации массивов:");
+//        Также можно сразу инициализировать массив таким образом
+        int[] newNumbers = {1,2,3};
+        for (int i = 0; i < newNumbers.length; i++){
+            System.out.println(newNumbers[i]);
+        }
+    }
+}
---
+++ src/DoWhile.java
@@ -0,0 +1,37 @@
+//      Урок 8: Цикл do...while.
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите 5")
+//        int value = scanner.nextInt();
+//        while(value != 5) {
+//            System.out.println("Введи 5");
+//            value = scanner.nextInt();
+//
+//        }
+
+//        То же самое можно написать с помощью цикла do-while:
+
+        /*
+         * Цикл do-while используется достаточно редко
+         * Отличие цикла do-while от while в том, что действие в do-while выполнится хотя бы 1 раз, не смотря на true/false.
+         */
+        Scanner scanner = new Scanner(System.in);
+        int value;
+
+
+
+        do {
+            System.out.println("Введи 5");
+            value = scanner.nextInt();
+        } while(value != 5);
+
+
+        System.out.println("Вы ввели 5");
+    }
+}
+
